edb9b8a651d7453f8069fe378cee24a33ef1bd96
Sithija Shehara <nsithijashehara@gmail.com>
2021-09-24 23:40:13 +0530

    login error fixed

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { auth } from "../firebase";
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
-  const handleLogIn = () => {};
+  const handleLogIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => console.log(result.user))
+      .catch((error) => console.log(error.message));
+  };
   return (
     <div>
       <div>Login</div>
       <br />
       <div>
-        <button onClick={handleLogIn()}>Google</button>
+        <button onClick={handleLogIn}>Google</button>
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,20 @@
-// import firebase from "firebase/app";
-// import "firebase/auth";
+// eslint - disable - next - line;
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCuJLtMixN7Ex_WnW71j7KHarzix6Jcl-o",
-//   authDomain: "invoice-app-bfe69.firebaseapp.com",
-//   projectId: "invoice-app-bfe69",
-//   storageBucket: "invoice-app-bfe69.appspot.com",
-//   messagingSenderId: "192413687575",
-//   appId: "1:192413687575:web:64932d03014817352be152",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCuJLtMixN7Ex_WnW71j7KHarzix6Jcl-o",
+  authDomain: "invoice-app-bfe69.firebaseapp.com",
+  projectId: "invoice-app-bfe69",
+  storageBucket: "invoice-app-bfe69.appspot.com",
+  messagingSenderId: "192413687575",
+  appId: "1:192413687575:web:64932d03014817352be152",
+};
 
-// export const auth = firebase.initializeApp(firebaseConfig).auth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
